b2c147c64530ea5f9f82eaa04dfb236754e31ba6
yoselin retamal <yoselin_yar@hotmail.com>
2022-04-21 19:38:27 -0400

    frasealeatorio

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
@@ -7,5 +6,46 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  console.log("trabajo");
+
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying";
+
+  //math.floor(math.random() * 2)
+  let numAleatorioWho = Math.floor(Math.random() * 4);
+  console.log(numAleatorioWho);
+  let aleatorioWho = who[numAleatorioWho];
+  console.log(aleatorioWho);
+
+  let numeroAleatorioAction = Math.floor(Math.random() * 4);
+  console.log(numeroAleatorioAction);
+  let aleatorioAction = action[numeroAleatorioAction];
+  console.log(aleatorioAction);
+
+  let numerosAleatoriosWhat = Math.floor(Math.random() * 3);
+  console.log(numerosAleatoriosWhat);
+  let aleatorioWhat = what[numerosAleatoriosWhat];
+  console.log(aleatorioWhat);
+
+  let numero1AleatorioWhen = Math.floor(Math.random() * 5);
+  console.log(numero1AleatorioWhen);
+  let aleatorioWhen = when[numero1AleatorioWhen];
+  console.log(aleatorioWhen);
+
+  let frases = aleatorioWho + aleatorioAction + aleatorioWhat + aleatorioWhen;
+  console.log(frases);
 };
